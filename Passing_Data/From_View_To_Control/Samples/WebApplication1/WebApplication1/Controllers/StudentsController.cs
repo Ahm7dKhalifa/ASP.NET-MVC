b5549090b4c98bd7feae4bc5f9572611a9a4e30c
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2017-12-14 03:05:29 +0200

    ADD Pasing_Data Chapter

---
+++ Passing_Data/From_View_To_Control/Samples/WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private SchoolDB db = new SchoolDB();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Age")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Students/CreateFirstWay
+        public ActionResult CreateFirstWay()
+        {
+            return View();
+        }
+
+        // POST: Students/CreateFirstWay
+        [HttpPost]
+        public ActionResult CreateFirstWayPost()
+        {
+            int Id = Convert.ToInt32(Request["Id"].ToString());
+            String Name = Request["Id"].ToString();
+            int Age = Convert.ToInt32(Request["Age"].ToString());
+
+            TempData["Id"] = Id;
+            TempData["Name"] = Name;
+            TempData["Age"] = Age;
+            return RedirectToAction("Result");
+        }
+
+        public ActionResult Result()
+        {
+            ViewBag.Id = TempData["Id"];
+            ViewBag.Name = TempData["Name"];
+            ViewBag.Age= TempData["Age"];
+
+            return View();
+        }
+
+        // GET: Students/CreateFirstWay
+        public ActionResult CreateSecondtWay()
+        {
+            return View();
+        }
+
+        // POST: Students/CreateFirstWay
+        [HttpPost]
+        public ActionResult CreateSecondWayPost(string Id,string Name,string Age)
+        {
+            int MyId = Convert.ToInt32(Id);
+            string MyName = Name;
+            int MyAge = Convert.ToInt32(Age);
+
+            TempData["Id"] = Id;
+            TempData["Name"] = Name;
+            TempData["Age"] = Age;
+            return RedirectToAction("Result");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
