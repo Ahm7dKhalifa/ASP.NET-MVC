f124dadc62c8a658a9e0001eee12e5ba1160ef47
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2019-03-23 18:06:44 +0200

    ADD External Or Social Authentication Project In Project Folder

---
+++ _Projects/External Or Social Authentication/Social_Login/Social_Login/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social_Login.Startup))]
+namespace Social_Login
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
